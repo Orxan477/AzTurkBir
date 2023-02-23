11ca45b42bc411258d05755efff6b6e1254a98a0
Orxan477 <tu201906147@code.edu.az>
2023-02-23 23:57:27 +0400

    Some change files

---
+++ Aztobir/Controllers/TeamController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aztobir.UI.Controllers
+{
+    public class TeamController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
