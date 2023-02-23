7a952520e06d7f7d2384ea3e6a7ca024b346dc64
Orxan477 <tu201906147@code.edu.az>
2023-02-23 12:26:20 +0400

    All page done

---
+++ Aztobir/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aztobir.UI.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Aztobir/Controllers/UniversityController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aztobir.UI.Controllers
+{
+    public class UniversityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
