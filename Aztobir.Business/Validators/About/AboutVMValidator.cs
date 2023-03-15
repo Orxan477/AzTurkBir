26976f0ea224491a102ade0146b8019798467c49
Orxan477 <tu201906147@code.edu.az>
2023-03-15 10:40:35 +0400

    Abot Update Validation added

---
+++ Aztobir.Business/Validators/About/AboutVMValidator.cs
@@ -0,0 +1,13 @@
+﻿using Aztobir.Business.ViewModels.About;
+using FluentValidation;
+
+namespace Aztobir.Business.Validators.About
+{
+    public class AboutVMValidator:AbstractValidator<AboutVM>
+    {
+        public AboutVMValidator()
+        {
+            RuleFor(x => x.Content).MaximumLength(1000);
+        }
+    }
+}
