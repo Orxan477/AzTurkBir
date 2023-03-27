edcfdb14372fda4ff3460e55c97d5da98d4bab56
Orxan477 <tu201906147@code.edu.az>
2023-03-27 09:45:15 +0400

    Dashboard Updated

---
+++ Aztobir.Business/ViewModels/Dashboard/DashboardVM.cs
@@ -0,0 +1,10 @@
+﻿namespace Aztobir.Business.ViewModels.Dashboard
+{
+    public class DashboardVM
+    {
+        public int Universities{ get; set; }
+        public int UniContacts { get; set; }
+        public int News { get; set; }
+        public int Contacts{ get; set; }
+    }
+}
--- Aztobir/Areas/admin/Controllers/DashboardController.cs
+++ Aztobir/Areas/admin/Controllers/DashboardController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Aztobir.Business.Interfaces;
+using Aztobir.Business.ViewModels.Dashboard;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Aztobir.UI.Areas.admin.Controllers
@@ -7,10 +9,27 @@ namespace Aztobir.UI.Areas.admin.Controllers
     [Authorize(Roles = "Admin")]
     public class DashboardController : Controller
     {
+        private IAztobirService _aztobirService;
+
+        public DashboardController(IAztobirService aztobirService)
+        {
+            _aztobirService = aztobirService;
+        }
     [Route("/admin/")]
-        public IActionResult Index()
-        {   
-            return View();
+        public async Task<IActionResult> Index()
+        {
+            var dbUniversitites = await _aztobirService.UniversityService.GetAll();
+            var dbUniversityContacts = await _aztobirService.UniversityFormService.GetAll();
+            var dbNews = await _aztobirService.NewsService.GetAll();
+            var dbContacts = await _aztobirService.ContactService.GetAll();
+            DashboardVM dashboard = new DashboardVM()
+            {
+                Universities = dbUniversitites.Count(),
+                UniContacts = dbUniversityContacts.Count(),
+                News = dbNews.Count(),
+                Contacts = dbContacts.Count()
+            };
+            return View(dashboard);
         }
     }
 }
