7858d4332dc37ce208a63929c8cfc1e63dcc4e9b
Orxan477 <tu201906147@code.edu.az>
2023-03-28 10:08:53 +0400

    Validation done

---
+++ Aztobir.Business/Validators/Faculty/FacultyCreateVMValidator.cs
@@ -0,0 +1,13 @@
+﻿using Aztobir.Business.ViewModels.Home.Faculty;
+using FluentValidation;
+
+namespace Aztobir.Business.Validators.Faculty
+{
+    public class FacultyCreateVMValidator:AbstractValidator<FacultyCreateVM>
+    {
+        public FacultyCreateVMValidator()
+        {
+            RuleFor(x => x.Name).MaximumLength(50).NotEmpty().NotNull();
+        }
+    }
+}
---
+++ Aztobir.Business/Validators/Faculty/FacultyUpdateVMValidator.cs
@@ -0,0 +1,13 @@
+﻿using Aztobir.Business.ViewModels.Home.Faculty;
+using FluentValidation;
+
+namespace Aztobir.Business.Validators.Faculty
+{
+    public class FacultyUpdateVMValidator:AbstractValidator<FacultyUpdateVM>
+    {
+        public FacultyUpdateVMValidator()
+        {
+            RuleFor(x => x.Name).MaximumLength(50);
+        }
+    }
+}
