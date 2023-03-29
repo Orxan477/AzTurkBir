dc814f5ea0118e851d8b8416e7dd8e3b85a479f5
Orxan477 <tu201906147@code.edu.az>
2023-03-29 14:35:59 +0400

    Fixed

--- Aztobir/Controllers/UniversityController.cs
+++ Aztobir/Controllers/UniversityController.cs
@@ -36,7 +36,7 @@ namespace Aztobir.UI.Controllers
                     Feedbacks = await _aztobirService.UniversityService.GetFeedbacks(id),
                     UniversityId = id,
                 };
-                return View(universityView);
+                return Json(universityView);
             }
             catch (Exception ex)
             {
